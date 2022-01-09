367e1a2f3860349127783dec44f0d7c5681daad9
aloysius-thomas <aloysiusthomasveliyath@gmail.com>
2022-01-09 21:42:48 +0530

    static root

--- project/settings.py
+++ project/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/home/nthindex/api-training/static-root/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
