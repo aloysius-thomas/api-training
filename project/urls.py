c2e3f9faeb024cff2972840be76a2ba1c4bfb573
aloysius-thomas <aloysiusthomasveliyath@gmail.com>
2022-01-09 21:45:59 +0530

    url change

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('accounts.api.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('', include('rest_framework.urls'))
 
 ]
