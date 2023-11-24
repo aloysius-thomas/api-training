6952423bb37d64c4231e87e4e094e7e090a93800
aloysius <aloysiuspaulthomas@gmail.com>
2023-11-24 19:31:38 +0530

    debug True

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tw1f^gt*440(^wuc4kd!5x4ke&fgmir54o6wsl)@^zzjghybo4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
